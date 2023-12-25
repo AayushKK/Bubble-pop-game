446292fa2de7b7cc79bca91c44351c5e944789dd
AayushKK <104511889+AayushKK@users.noreply.github.com>
2023-12-25 19:25:44 +0545

    Add files via upload

---
+++ Bubble pop game/bubble.js
@@ -0,0 +1,96 @@
+const bubbleContainer = document.getElementById('bubble-container');
+const scoreElement = document.getElementById('score-value');
+    let score = 0;
+    let flag =false;
+
+    let colors={'red': 5,
+                'blue': 2,
+                'green': 4,
+                'yellow':3,
+                'orange':7,
+                'purple':6,
+                'pink': 8};
+
+
+   function genRandomColor() {
+                  // get random color name
+                  let  colorname = Object.keys(colors);
+                  let randomColor = colorname[Math.floor(Math.random() * colorname.length)];
+                   return randomColor;
+   }
+  
+  function createBubble() {
+    
+        // create a bubble
+      const bubble = document.createElement('div');
+      bubble.classList.add('bubble');
+        // set bubble position
+      let container = bubbleContainer.getBoundingClientRect();
+      let containerLeft = container.left;
+      let containerTop = container.top;
+      let containerWidth = container.width;
+      let containerHeight = container.height;
+      bubble.style.left = containerLeft + Math.floor(Math.random() * (containerWidth - 50)) + "px";
+      bubble.style.top = containerTop + Math.floor(Math.random() * (containerHeight - 50)) + "px";
+         // set bubble color
+      bubble.style.backgroundColor = genRandomColor();
+            // remove bubble when clicked
+      bubble.addEventListener('click', function () 
+      {
+        let currentColor = bubble.style.backgroundColor;
+            // update score
+        score+=colors[currentColor];
+        document.body.removeChild(bubble);
+        scoreElement.textContent = score;
+            // check if game is over
+        if(score >=20){
+                
+            flag=true;
+            
+
+        }
+       
+        
+      });
+      
+      document.body.appendChild(bubble);
+            // game over
+      if(flag){
+        clearInterval(startGame);
+                // create game over message
+            let endofgame = document.createElement('div');
+          endofgame.classList.add('endgame');
+          endofgame.innerHTML='Game Over';
+          endofgame.style.color = 'green';
+          endofgame.style.fontSize = 100 + "px";
+          endofgame.style.fontWeight = 'bold';
+          
+                // remove all bubbles
+          const bubble = document.getElementsByClassName('bubble');  
+          let bubbleArray=Array.from(bubble);
+          bubbleArray.forEach(function(item){
+            document.body.removeChild(item);
+          });
+                // display game over message
+          bubbleContainer.innerHTML=endofgame.outerHTML;
+          bubbleContainer.style.alignItems = 'center';
+          bubbleContainer.style.justifyContent = 'center';
+          bubbleContainer.style.paddingLeft = 20+'px';
+          bubbleContainer.style.paddingTop = 100+'px';
+          
+
+      }
+              
+  }
+      
+  
+        // start game
+    let startGame = setInterval(createBubble, 1000)
+  
+
+  
+  
+
+  
+  
+  
